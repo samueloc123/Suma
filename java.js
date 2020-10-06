86b82c64c3eb0e4ede5789a8854ea9879337eb2f
samueloc123 <72421709+samueloc123@users.noreply.github.com>
2020-10-05 19:28:04 -0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,44 @@
+var ques=function(){
+	var n1=document.getElementById("papa");
+	var n2=document.getElementById("hijo2");
+var n3=document.getElementById("hijo3");
+var n4=document.getElementById("hijo4");
+var n5=document.getElementById("hijo5");
+var n6=document.getElementById("hijo6");
+var n7=document.getElementById("hijo7");
+var n8=document.getElementById("im");
+
+	console.log("hol");
+
+	n3.className="cuadro55";
+		n8.className="ima";
+
+}
+
+var resolver=function(){
+	var n1=document.getElementById("papa");
+	var n2=document.getElementById("hijo2");
+var n3=document.getElementById("hijo3");
+var n4=document.getElementById("hijo4");
+var n5=document.getElementById("hijo5");
+var n6=document.getElementById("hijo6");
+var n7=document.getElementById("hijo7");
+var n8=document.getElementById("hijo31");
+
+
+	console.log("hola");
+
+	n8.className="cuadro56";n3.className="cuadro57";
+
+}
+
+var calculo=function(){
+	var n1=document.getElementById("b1").value;
+	var n2=document.getElementById("b2").value;
+var n3=document.getElementById("b3").value;
+var n4=document.getElementById("boton");
+
+  suma=parseInt(n1)+parseInt(n2)-parseInt(n3);
+console.log(suma);
+alert("suma: "+suma);
+}
